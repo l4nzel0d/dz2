55b78c97f6e72f6d552160d31f77ab111bdebc76
l4nzel0d <borisych095@yandex.ru>
2024-10-26 22:39:33 +0000

    config.xml tweak: changed package name > visualizer.py feat: add generate_dependencies_graph, add visualize_graph

--- visualizer.py
+++ visualizer.py
@@ -15,9 +15,35 @@ def read_config(config_path):
 		print("Error Parsing XML file")
 		sys.exit(1)
 
+def generate_dependencies_graph(package_name):
+	try:
+		result = subprocess.run(["apk", "dot", package_name], capture_output=True, text=True, check=True)
+		return result.stdout
+	except subprocess.CalledProcessError:
+		print("Error executing apk command.")
+		sys.exit(1)
+
+def visualize_graph(graph_data, visualizer_path, package_name):
+	with tempfile.NamedTemporaryFile(delete=False, suffix=".dot") as dot_file:
+		dot_file.write(graph_data.encode('utf-8'))
+		dot_path = dot_file.name
+	
+	output_path = f"{package_name}_dependencies.png"
+	try:
+		subprocess.run([visualizer_path, "-Tpng", dot_path, "-o", output_path], check=True)
+		print(f"Dependency graph saved to {output_path}")
+		
+		# Open the generated image in Ristretto
+		subprocess.run(["ristretto", output_path])
+		
+	finally:
+		os.remove(dot_path)
+
 def main(config_path):
 	visualizer_path, package_name = read_config(config_path)
-	print(visualizer_path, package_name)
+	graph_data = generate_dependencies_graph(package_name)
+	visualize_graph(graph_data, visualizer_path, package_name)
+
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
